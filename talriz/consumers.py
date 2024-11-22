3917eea214f56400a0313b3beeb3c0dfbca40aab
Tiffany Zheng <tiffanyzheng@Tiffanys-MacBook-Air.local>
2024-11-22 17:35:31 -0500

    multiple clients using set()

--- talriz/consumers.py
+++ talriz/consumers.py
@@ -1,20 +1,28 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+Clients = set()
 class NotificationConsumer(WebsocketConsumer):
+    
     def connect(self):
+        Clients.add(self)
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        Clients.discard(self)
+        
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message', '')
+        for users in Clients: 
+            users.send(text_data = json.dumps({
+                'message': message
+            }))
 
-        # Send a response back to the client
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+       
+        # self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
 
 # This probably should goto the backend
