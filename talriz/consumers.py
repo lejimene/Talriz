68f4b6a3d0ae133395f17f31b2df3bdcdc1f5dd9
Tiffany Zheng <tiffanyzheng@Tiffanys-MacBook-Air.local>
2024-11-22 17:54:23 -0500

    changed from set() to a list

--- talriz/consumers.py
+++ talriz/consumers.py
@@ -1,21 +1,23 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
 
-Clients = set()
+Clients = []
 class NotificationConsumer(WebsocketConsumer):
-    
+
     def connect(self):
-        Clients.add(self)
+        if self not in Clients: 
+            Clients.append(self)
         self.accept()
 
     def disconnect(self, close_code):
-        Clients.discard(self)
+        if self in Clients: 
+            Clients.remove(self)
         
 
-    def receive(self, text_data):
+    def receive(self, text_data): 
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message', '')
-        for users in Clients: 
+        for users in Clients[:]: 
             users.send(text_data = json.dumps({
                 'message': message
             }))
